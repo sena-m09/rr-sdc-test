b9d8b91c9bfce56f0689247c9e1121bdbe21d058
sena-m09 <sena-murakami@gaji.jp>
2025-02-11 20:18:45 +0900

    users をコンソールに表示

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { users } from '../db/schema';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,10 +9,16 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
+export async function loader({ context }: Route.LoaderArgs) {
+  const message = context.cloudflare.env.VALUE_FROM_CLOUDFLARE ;
+  const res = await fetch("http://localhost:5173/api/users/get");
+  const users = await res.json();
+
+  return { message, users };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
+  const users = loaderData.users;
+  console.log(users);
   return <Welcome message={loaderData.message} />;
 }
